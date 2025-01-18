1caad5347edb485a358acf935dc6f04a5fdebfab
protap1100 <protapb23@gmail.com>
2025-01-18 21:50:18 +0600

    router setup done

---
+++ src/routes/Router.jsx
@@ -0,0 +1,8 @@
+import App from "../App";
+import { createBrowserRouter } from "react-router-dom";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
